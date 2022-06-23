6ce82118a9e0d5a1e69d928b0b0b5dbdc873264b
Chris Fryer <chris.fryer@digital.cabinet-office.gov.uk>
2022-06-23 12:33:06 +0100

    Integrate with GOV.UK Sign In
    Uses openid-client and passport.js

--- app/routes.js
+++ app/routes.js
@@ -1,29 +1,92 @@
-const express = require('express')
+const express = require('express');
 const router = express.Router()
 // Add your routes here - above the module.exports line
-const { auth, requiresAuth } = require('express-openid-connect');
-router.use(
-  auth({
-    authRequired: false,
-    idpLogout: true,
-    routes: {
-        login: false,
-        callback: '/callback', // I think this is a dummy URL
-     },
-    authorizationParams: {
-        scope: 'openid email profile',
-    }
-  })
-);
-// requiresAuth checks authentication.
-router.get('/login', (req, res) => res.oidc.login(
-  { returnTo: '/profile' }
-));
-router.use((req, res, next) => {
-  res.locals.user = req.oidc.user;
-  res.locals.userProfile = JSON.stringify(req.oidc.user);
-  next();
-});
-router.get('/profile', requiresAuth());
+const passport = require('passport');
+const { Issuer, Strategy, generators, custom } = require('openid-client');
+const pem2jwk = require('rsa-pem-to-jwk');
 
-module.exports = router
+// These keys are base64 encoded in .env
+const privatekey = Buffer.from(process.env.RSA_PRIVATE_KEY, 'base64').toString('utf8').replace(/\\n/gm, '\n');
+const publickey  = Buffer.from(process.env.RSA_PUBLIC_KEY,  'base64').toString('utf8').replace(/\\n/gm, '\n');;
+const cert       = Buffer.from(process.env.CERT,            'base64').toString('utf8').replace(/\\n/gm, '\n');;
+const jwk        = pem2jwk(privatekey, { kid: '2022-06-ova-alpha', use: 'sig'} , 'private');
+
+Issuer.discover(process.env.ISSUER_BASE_URL).then(issuer => {
+
+  //console.log(issuer);
+
+  const client = new issuer.FAPI1Client({
+    client_id: process.env.CLIENT_ID,
+    redirect_uris: [ process.env.CALLBACK_URL ],
+    response_types: [ 'code' ],
+    token_endpoint_auth_method: 'private_key_jwt',
+    id_token_signed_response_alg: 'ES256', // Great Caesar's ghost! It was this.
+  },{
+    keys: [ jwk ],
+  });
+
+  client[custom.http_options] = function() {
+    const result = {};
+    result.cert = cert;
+    result.key = privatekey;
+    return result;
+  }
+
+  //console.log(client);
+
+  router.use(passport.initialize());
+  router.use(passport.session());
+
+  passport.use(
+    'oidc',
+    new Strategy({
+      client: client,
+      params: {
+        scope: 'openid email phone',
+        nonce: generators.nonce(),
+      },
+      passReqToCallback: true,
+      sessionKey: 'data',
+    }, (req, tokenset, userinfo, done) => {
+
+      /* TODO: Perform some checks */
+
+      if (userinfo.sub) {
+        console.log('sub: ', userinfo.sub, ' logged in');
+        return done(null, userinfo);
+      } else {
+        return done('Userinfo not found. Check the logs.');
+      }
+    })
+  );
+
+  router.get('/login', (req, res, next) => {
+    passport.authenticate('oidc')(req, res, next);
+  });
+
+  router.get('/callback', (req, res, next) => {
+    passport.authenticate('oidc', { 
+      successRedirect: '/profile',
+      successMessage: true,
+      failureRedirect: '/login', /* this may go loopy */
+      failureMessage: true,
+    })(req, res, next);
+  });
+
+  router.use((req, res, next) => {
+    // console.log('req.user: ', req.user);
+    res.locals.user = req.user;
+    next();
+  });
+
+  passport.serializeUser(function(user, cb) {
+    cb(null, user);
+  });
+
+  passport.deserializeUser(function(obj, cb) {
+    cb(null, obj);
+  });
+
+});
+
+module.exports = router;
